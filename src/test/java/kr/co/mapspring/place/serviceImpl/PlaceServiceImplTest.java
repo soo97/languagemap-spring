e62b4616c8b7e59c92e2f5318a45db224867c8f7
hongsoonchan02 <asm640789@gmail.com>
2026-04-21 11:57:54 +0900

    feat(place) : test 코드 클래스 추가

---
+++ src/test/java/kr/co/mapspring/place/serviceImpl/PlaceServiceImplTest.java
@@ -0,0 +1,19 @@
+package kr.co.mapspring.place.serviceImpl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PlaceServiceImplTest {
+
+	@Test
+	void testSavePlace() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPlaceServiceImpl() {
+		fail("Not yet implemented");
+	}
+
+}
