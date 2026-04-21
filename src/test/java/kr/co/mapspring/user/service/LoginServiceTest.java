caf7abe7333e0058ce5ad02f164e1cff02fec67f
tig <leehjpp4@gmail.com>
2026-04-21 15:37:27 +0900

    feat(user): Service 테스트 코드 작성

---
+++ src/test/java/kr/co/mapspring/user/service/LoginServiceTest.java
@@ -0,0 +1,35 @@
+package kr.co.mapspring.user.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@ExtendWith(MockitoExtension.class)
+public class LoginServiceTest {
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @InjectMocks
+    private LoginService loginService;
+    
+    @Test
+    @DisplayName("존재하는 이메일과 올바른 비밀번호면 로그인에 성공한다")
+    void loginSuccess() {
+        // given
+
+        // when
+
+        // then
+    	
+    	
+    	
+    }
+
+}
