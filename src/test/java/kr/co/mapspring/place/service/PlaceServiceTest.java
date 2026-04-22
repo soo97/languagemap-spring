6ec12711f82f5684767dba20d274f9170b2e34e3
ixxveon <igayeon322s@gmail.com>
2026-04-22 15:39:02 +0900

    feat(learning): 목표 선택 기능 및 테스트 ê코 구현

--- src/test/java/kr/co/mapspring/place/service/PlaceServiceTest.java
+++ src/test/java/kr/co/mapspring/place/service/PlaceServiceTest.java
@@ -8,7 +8,6 @@ import static org.mockito.Mockito.when;
 import java.math.BigDecimal;
 import java.util.Optional;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
