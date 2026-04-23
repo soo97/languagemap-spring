696ead1c2105c3fbb503fe6be5847ecfe6520646
tig <leehjpp4@gmail.com>
2026-04-23 16:42:20 +0900

    feat(user): 성공 테스트를 위한 서비스 로직 구현 및 테스트 코드 구현중

--- src/main/java/kr/co/mapspring/user/service/impl/SignUpServiceImpl.java
+++ src/main/java/kr/co/mapspring/user/service/impl/SignUpServiceImpl.java
@@ -1,6 +1,11 @@
 package kr.co.mapspring.user.service.impl;
 
+import java.time.LocalDate;
+
+import kr.co.mapspring.global.exception.CustomException;
+import kr.co.mapspring.global.exception.ErrorCode;
 import kr.co.mapspring.user.dto.SignUpDto;
+import kr.co.mapspring.user.entity.User;
 import kr.co.mapspring.user.repository.UserRepository;
 import kr.co.mapspring.user.service.SignUpService;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +22,29 @@ public class SignUpServiceImpl implements SignUpService {
 
     @Override
     public SignUpDto.ResponseSignUp signUp(SignUpDto.RequestSignUp request) {
-        return null;
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new CustomException(ErrorCode.DUPLICATE_RESOURCE, "이미 존재하는 이메일입니다.");
+        }
+
+        if (!request.getPassword().equals(request.getPasswordConfirm())) {
+            throw new CustomException(ErrorCode.BAD_REQUEST, "비밀번호와 비밀번호 확인이 일치하지 않습니다.");
+        }
+
+        String encodedPassword = passwordEncoder.encode(request.getPassword());
+
+        LocalDate birthDate = LocalDate.parse(request.getBirthDate());
+
+        User user = User.create(
+                request.getEmail(),
+                request.getName(), 	
+                birthDate,
+                request.getAddress(),
+                request.getPhoneNumber(),
+                encodedPassword
+        );
+
+        User savedUser = userRepository.save(user);
+
+        return SignUpDto.ResponseSignUp.from(savedUser);
     }
 }
