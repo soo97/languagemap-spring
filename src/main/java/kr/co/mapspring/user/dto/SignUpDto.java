5e84bae78a3a8a37b383b86a8defb9667447695e
tig <leehjpp4@gmail.com>
2026-04-23 14:56:23 +0900

    feat(user): 회원가입 DTO 생성

---
+++ src/main/java/kr/co/mapspring/user/dto/SignUpDto.java
@@ -0,0 +1,64 @@
+package kr.co.mapspring.user.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import kr.co.mapspring.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class SignUpDto {
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Schema(description = "회원가입 요청 DTO")
+    public static class RequestSignUp {
+
+        @Schema(description = "사용자 이름", example = "홍길동")
+        private String name;
+
+        @Schema(description = "생년월일", example = "2000-01-01")
+        private String birthDate;
+
+        @Schema(description = "주소", example = "서울시 강남구")
+        private String address;
+
+        @Schema(description = "전화번호", example = "010-1234-5678")
+        private String phoneNumber;
+
+        @Schema(description = "이메일", example = "test@naver.com")
+        private String email;
+
+        @Schema(description = "비밀번호", example = "1234")
+        private String password;
+
+        @Schema(description = "비밀번호 확인", example = "1234")
+        private String passwordConfirm;
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Schema(description = "회원가입 응답 DTO")
+    public static class ResponseSignUp {
+
+        @Schema(description = "사용자 ID", example = "1")
+        private Long userId;
+
+        @Schema(description = "이메일", example = "test@naver.com")
+        private String email;
+
+        @Schema(description = "이름", example = "홍길동")
+        private String name;
+
+        public static ResponseSignUp from(User user) {
+            return ResponseSignUp.builder()
+                    .userId(user.getUserId())
+                    .email(user.getEmail())
+                    .name(user.getName())
+                    .build();
+        }
+    }
+}
