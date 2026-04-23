211a2bdc9dfdbd109fa612c453f4b2aec1a9c2b2
tig <leehjpp4@gmail.com>
2026-04-23 17:43:11 +0900

    feat(user): 회원가입 서비스 로직 예외처리 코드 적용

--- src/main/java/kr/co/mapspring/user/service/impl/SignUpServiceImpl.java
+++ src/main/java/kr/co/mapspring/user/service/impl/SignUpServiceImpl.java
@@ -2,15 +2,16 @@ package kr.co.mapspring.user.service.impl;
 
 import java.time.LocalDate;
 
-import kr.co.mapspring.global.exception.CustomException;
-import kr.co.mapspring.global.exception.ErrorCode;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import kr.co.mapspring.global.exception.user.EmailAlreadyExistsException;
+import kr.co.mapspring.global.exception.user.PasswordConfirmMismatchException;
 import kr.co.mapspring.user.dto.SignUpDto;
 import kr.co.mapspring.user.entity.User;
 import kr.co.mapspring.user.repository.UserRepository;
 import kr.co.mapspring.user.service.SignUpService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
@@ -23,11 +24,11 @@ public class SignUpServiceImpl implements SignUpService {
     @Override
     public SignUpDto.ResponseSignUp signUp(SignUpDto.RequestSignUp request) {
         if (userRepository.existsByEmail(request.getEmail())) {
-            throw new CustomException(ErrorCode.DUPLICATE_RESOURCE, "이미 존재하는 이메일입니다.");
+        	throw new EmailAlreadyExistsException();
         }
 
         if (!request.getPassword().equals(request.getPasswordConfirm())) {
-            throw new CustomException(ErrorCode.BAD_REQUEST, "비밀번호와 비밀번호 확인이 일치하지 않습니다.");
+        	throw new PasswordConfirmMismatchException();
         }
 
         String encodedPassword = passwordEncoder.encode(request.getPassword());
