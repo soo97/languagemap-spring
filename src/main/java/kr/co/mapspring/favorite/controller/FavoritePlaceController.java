abe941abb793c45fc0b0cdb2d71fdf1fac009701
ixxveon <igayeon322s@gmail.com>
2026-04-24 11:35:35 +0900

    feat(favorite): FavoritePlaceController 즐겨찾기 추가/삭제/조회 구현

---
+++ src/main/java/kr/co/mapspring/favorite/controller/FavoritePlaceController.java
@@ -0,0 +1,47 @@
+package kr.co.mapspring.favorite.controller;
+
+import kr.co.mapspring.favorite.dto.FavoritePlaceDto;
+import kr.co.mapspring.favorite.entity.FavoritePlace;
+import kr.co.mapspring.favorite.service.FavoritePlaceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/favorites/places")
+@RequiredArgsConstructor
+public class FavoritePlaceController {
+
+    private final FavoritePlaceService favoritePlaceService;
+
+    @PostMapping
+    public void addFavoritePlace(@RequestBody FavoritePlaceDto.RequestAddFavoritePlace request) {
+        favoritePlaceService.addFavoritePlace(
+                request.getUserId(),
+                request.getPlaceId()
+        );
+    }
+
+    @DeleteMapping
+    public void removeFavoritePlace(@RequestBody FavoritePlaceDto.RequestRemoveFavoritePlace request) {
+        favoritePlaceService.removeFavoritePlace(
+                request.getUserId(),
+                request.getPlaceId()
+        );
+    }
+
+    @GetMapping
+    public List<FavoritePlaceDto.ResponseFavoritePlace> getFavoritePlaces(@RequestBody Long userId) {
+        List<FavoritePlace> favoritePlaces = favoritePlaceService.getFavoritePlaces(userId);
+
+        return favoritePlaces.stream()
+                .map(FavoritePlaceDto.ResponseFavoritePlace::from)
+                .toList();
+    }
+}
