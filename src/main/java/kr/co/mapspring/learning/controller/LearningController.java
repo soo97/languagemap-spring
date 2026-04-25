80ea8b356298fc2594338d8aa6e9d6556620677b
ixxveon <igayeon322s@gmail.com>
2026-04-26 01:37:02 +0900

    feat(learning): 학습 기록 조회 AController 및dSwagger문서 추가

---
+++ src/main/java/kr/co/mapspring/learning/controller/LearningController.java
@@ -0,0 +1,29 @@
+package kr.co.mapspring.learning.controller;
+
+import kr.co.mapspring.global.dto.ApiResponseDTO;
+import kr.co.mapspring.learning.controller.docs.LearningControllerDocs;
+import kr.co.mapspring.learning.dto.LearningLogDto;
+import kr.co.mapspring.learning.service.LearningService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/learning")
+@RequiredArgsConstructor
+public class LearningController implements LearningControllerDocs {
+
+    private LearningService learningService;
+
+    @GetMapping("logs")
+    public ResponseEntity<ApiResponseDTO<List<LearningLogDto.ResponseLog>>> getStudyLogs(@RequestParam("userId") Long userId) {
+        List<LearningLogDto.ResponseLog> result = learningService.getStudyLogs(userId);
+
+        return ResponseEntity.ok(ApiResponseDTO.success("학습 기록 조회 성공", result));
+    }
+}
