7217e5cc31eae05111f3dd5e2aaeee6ebbd3f0b1
hyun <syjk7503@gmail.com>
2026-04-29 17:52:40 +0900

    feat(config) : @Asylc 어노테이션을 위한 config 생성

---
+++ src/main/java/kr/co/mapspring/global/config/AsyncConfig.java
@@ -0,0 +1,10 @@
+package kr.co.mapspring.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync // @Async 활성화
+public class AsyncConfig {
+
+}
