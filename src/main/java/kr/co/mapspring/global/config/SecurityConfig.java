03faab3480841913c6e1287505e8527b3fe2241b
tig <leehjpp4@gmail.com>
2026-04-29 17:00:35 +0900

    refactor: 서버포트맞추기

--- src/main/java/kr/co/mapspring/global/config/SecurityConfig.java
+++ src/main/java/kr/co/mapspring/global/config/SecurityConfig.java
@@ -136,7 +136,11 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "http://localhost:80",
+                "http://localhost"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
