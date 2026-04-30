9294fb76ea1f5ba4a9ae954685cbf5bf1c32d6b7
ixxveon <igayeon322s@gmail.com>
2026-04-30 14:56:10 +0900

    feat(chat): 실시간 채팅 ErrorCode 추가

--- src/main/java/kr/co/mapspring/chat/controller/ChatController.java
+++ src/main/java/kr/co/mapspring/chat/controller/ChatController.java
@@ -2,12 +2,15 @@ package kr.co.mapspring.chat.controller;
 
 import kr.co.mapspring.chat.controller.docs.ChatControllerDocs;
 import kr.co.mapspring.chat.dto.ChatMessageDto;
+import kr.co.mapspring.global.exception.chat.InvalidChatMessageException;
 import kr.co.mapspring.user.entity.User;
 import kr.co.mapspring.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+
+import static io.lettuce.core.pubsub.PubSubOutput.Type.message;
 
 @Controller
 @RequiredArgsConstructor
@@ -21,9 +24,17 @@ public class ChatController implements ChatControllerDocs {
     public ChatMessageDto.ResponseMessage sendMessage(
             ChatMessageDto.RequestSendMessage request
     ) {
+        validateMessage(request.getMessage());
+
         User user = userRepository.findById(request.getUserId())
                 .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
 
         return ChatMessageDto.ResponseMessage.of(user, request.getMessage());
+    }
+
+    private void validateMessage(String message) {
+        if (message == null || message.isBlank()) {
+            throw new InvalidChatMessageException();
+        }
     }
 }
