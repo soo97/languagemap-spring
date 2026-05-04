854a9550c88120ce74f7b7be9efad6503581f7cc
hongsoonchan02 <asm640789@gmail.com>
2026-05-04 13:56:33 +0900

    fix(place): 스웨거 api 불일치 수정

--- src/main/java/kr/co/mapspring/place/repository/LearningSessionRepository.java
+++ src/main/java/kr/co/mapspring/place/repository/LearningSessionRepository.java
@@ -6,13 +6,10 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import kr.co.mapspring.place.entity.LearningSession;
-import kr.co.mapspring.place.enums.MissionSessionStatus;
 
 public interface LearningSessionRepository extends JpaRepository<LearningSession, Long> {
 
     @EntityGraph(attributePaths = {"place", "place.region", "place.scenario"})
     Optional<LearningSession> findBySessionId(Long sessionId);
-    
-    boolean existsByLearningSession_SessionIdAndMissionStatusNot(Long sessionId, MissionSessionStatus missionStatus);
     
 }
