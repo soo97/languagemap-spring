ddfe785fd2aff9dd5d29ad7af154106c63409a52
ixxveon <igayeon322s@gmail.com>
2026-05-11 16:46:38 +0900

    fix(ranking): JPQL named parameter 매핑 수정

--- src/main/java/kr/co/mapspring/ranking/repository/RankingRepository.java
+++ src/main/java/kr/co/mapspring/ranking/repository/RankingRepository.java
@@ -3,6 +3,7 @@ package kr.co.mapspring.ranking.repository;
 import kr.co.mapspring.learning.entity.StudyScore;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -24,21 +25,21 @@ public interface RankingRepository extends JpaRepository<StudyScore, Long> {
            GROUP BY ss.studyLog.user.userId, ss.studyLog.user.name
            ORDER BY SUM(ss.totalScore) DESC
            """)
-    List<Object[]> findFriendRanking(List<Long> friendIds);
+    List<Object[]> findFriendRanking(@Param("friendIds") List<Long> friendIds);
 
     @Query("""
            SELECT COALESCE(MAX(ss.totalScore), 0)
            FROM StudyScore ss
            WHERE ss.studyLog.user.userId IN :friendIds
            """)
-    Long findFriendBestScore(List<Long> friendIds);
+    Long findFriendBestScore(@Param("friendIds") List<Long> friendIds);
 
     @Query("""
            SELECT COALESCE(AVG(ss.totalScore), 0)
            FROM StudyScore ss
            WHERE ss.studyLog.user.userId IN :friendIds
            """)
-    Double findFriendAverageScore(List<Long> friendIds);
+    Double findFriendAverageScore(@Param("friendIds") List<Long> friendIds);
 
     @Query("""
        SELECT ss.studyLog.user.userId, ss.studyLog.user.name, SUM(ss.totalScore)
@@ -47,5 +48,5 @@ public interface RankingRepository extends JpaRepository<StudyScore, Long> {
        GROUP BY ss.studyLog.user.userId, ss.studyLog.user.name
        ORDER BY SUM(ss.totalScore) DESC
        """)
-    List<Object[]> findWeeklyRanking(LocalDateTime startDateTime);
+    List<Object[]> findWeeklyRanking(@Param("startDateTime") LocalDateTime startDateTime);
 }
