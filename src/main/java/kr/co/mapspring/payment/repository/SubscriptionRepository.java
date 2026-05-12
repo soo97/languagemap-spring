ef856d658a782b4a46b26ef3f37e1588395a230b
tig <leehjpp4@gmail.com>
2026-05-12 17:16:15 +0900

    feat(payment): 관리자 결제내역관리 api 추가

--- src/main/java/kr/co/mapspring/payment/repository/SubscriptionRepository.java
+++ src/main/java/kr/co/mapspring/payment/repository/SubscriptionRepository.java
@@ -1,12 +1,18 @@
 package kr.co.mapspring.payment.repository;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import kr.co.mapspring.payment.entity.Payment;
 import kr.co.mapspring.payment.entity.Subscription;
 import kr.co.mapspring.payment.enums.SubscriptionStatus;
 import kr.co.mapspring.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
 
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
+	
     Optional<Subscription> findByPayment_UserAndPlanStatus(User user, SubscriptionStatus status);
+    
+    Optional<Subscription> findByPayment(Payment payment);
+    long countByPlanStatus(SubscriptionStatus planStatus);
 }
